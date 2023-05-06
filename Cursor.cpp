d2e378c1d004dd9afd6ade709ec786c0271198b7
IshanBhanuka <e18324@eng.pdn.ac.lk>
2023-05-07 01:21:33 +0530

    Uploading basic code of cursor control

---
+++ Cursor.cpp
@@ -0,0 +1,34 @@
+#include <windows.h>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	while (1)
+	{
+		cout<<"Enter cmd"<<endl;
+        char g = getch();
+		int x, y;
+		POINT xypos;
+
+        if (g == 'S' || g == 's')
+		{
+			cout<<"Enter the new position:"<<endl;
+			cin>>x>>y;
+			SetCursorPos(x, y);
+		}
+
+		else if (g == 'g' || g == 'G')
+		{
+			GetCursorPos(&xypos);
+			cout<<"X:"<<xypos.x<<"\tY:"<<xypos.y<<endl;
+		}
+
+
+
+
+    }
+
+}
