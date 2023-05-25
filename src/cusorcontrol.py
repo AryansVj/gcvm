4195e496e3dd7e0af08d94f25e9da4e545340621
IshanBhanuka <e18324@eng.pdn.ac.lk>
2023-05-25 15:30:18 +0530

    cousor control code by python

---
+++ src/cusorcontrol.py
@@ -0,0 +1,21 @@
+from pynput.mouse import Button,Controller
+mouse = Controller()
+
+#take the position of the mouse
+pos = mouse.position
+x = int(input("Give the x coordinate of the mouse: "))
+y = int(input("Give the y coordinate of the mouse: "))
+
+mouse.move(x,y)
+print(pos)
+
+# click the mouse
+time = 1         # time the button clicks
+mouse.click(Button.right, time)
+mouse.click(Button.left, time)
+
+'''
+# click nonstop
+mouse.press(Button.right)
+mouse.press(Button.right)
+'''
